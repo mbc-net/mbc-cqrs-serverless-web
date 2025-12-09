d00659f9c840b53fd5401d75d3cbc8a061b59018
Quan Nguyen Ba <quan.nguyenba@pionero.io>
2025-12-09 16:12:38 +0700

    feat: export survey creator

--- packages/survey/src/index.ts
+++ packages/survey/src/index.ts
@@ -2,3 +2,5 @@
 export { SurveyTemplatePage } from './modules/survey-template/templates/index'
 export { EditSurveyTemplatePage } from './modules/edit-survey-template/templates/index'
 export { SurveyForm } from './forms/survey-form'
+// export survey creator
+export { SurveyCreator } from './creators/survey-creator'
--- packages/survey/tsup.config.ts
+++ packages/survey/tsup.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     EditSurveyTemplatePage:
       'src/modules/edit-survey-template/templates/index.tsx',
     SurveyForm: 'src/forms/survey-form.tsx',
+    SurveyCreator: 'src/creators/survey-creator.tsx',
   },
   format: ['cjs', 'esm'],
   dts: true,
